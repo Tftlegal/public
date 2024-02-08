b279ac74761594119098a978ebfd87f2e1e68c6a
Tftlegal <94695082+Tftlegal@users.noreply.github.com>
2024-02-09 02:24:35 +0300

    Create index.php
    logo

---
+++ awesome-app/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Awesome app</title>
+  </head>
+  <body>
+  <h1>Welcome to your awesome app !</h1>
+
+  <p> App backend <?=gethostname()?> </p>
+
+    <img src="http://sytes.ru/assets/img/logo.jpg" />
+  </body>
+</html>
